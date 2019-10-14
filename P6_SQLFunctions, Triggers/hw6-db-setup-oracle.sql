082416c99e57acfd684077c506a7009e42ea6241
ShiboXing <shx26@pitt.edu>
2019-10-14 12:16:53 -0400

    added function project

---
+++ P6_SQLFunctions, Triggers/hw6-db-setup-oracle.sql
@@ -0,0 +1,212 @@
+----CS1555/2055 - DATABASE MANAGEMENT SYSTEMS (FALL 2019)
+----DEPT. OF COMPUTER SCIENCE, UNIVERSITY OF PITTSBURGH
+----ASSIGNMENT #6: SQL/PL: FUNCTIONS, PROCEDURES, AND TRIGGERS
+
+--Clean up
+drop table report cascade constraints;
+drop table coverage cascade constraints;
+drop table intersection cascade constraints;
+drop table road cascade constraints;
+drop table sensor cascade constraints;
+drop table worker cascade constraints;
+drop table forest cascade constraints;
+drop table state cascade constraints;
+drop table emergency cascade constraints;
+
+commit;
+
+--Create tables
+create table FOREST
+(
+    Forest_No  varchar(10),
+    Name       varchar(30),
+    Area       float,
+    Acid_Level float,
+    MBR_XMin   float,
+    MBR_XMax   float,
+    MBR_YMin   float,
+    MBR_YMax   float,
+    Constraint forest_PK primary key (Forest_No) not deferrable
+);
+
+create table STATE
+(
+    Name         varchar(30),
+    Abbreviation varchar(2),
+    Area         float,
+    Population   int,
+    Constraint State_PK primary key (Abbreviation) not deferrable
+);
+
+create table COVERAGE
+(
+    Forest_No  varchar(10),
+    State      varchar(2),
+    Percentage float,
+    Area       float,
+    Constraint coverage_PK primary key (Forest_No, State) not deferrable,
+    Constraint coverage_FK1 foreign key (Forest_No) references FOREST (Forest_No) initially deferred deferrable,
+    Constraint coverage_FK2 foreign key (State) references State (Abbreviation) initially deferred deferrable
+);
+
+create table ROAD
+(
+    Road_No varchar(10),
+    Name    varchar(30),
+    Length  float,
+    Constraint road_PK primary key (Road_No) not deferrable
+);
+
+create table INTERSECTION
+(
+    Forest_No varchar(10),
+    Road_No   varchar(10),
+    Constraint intersection_PK primary key (Forest_No, Road_No) not deferrable,
+    Constraint intersection_FK1 foreign key (Forest_No) references FOREST (Forest_No) initially deferred deferrable,
+    Constraint intersection_FK2 foreign key (Road_No) references ROAD (Road_No) initially deferred deferrable
+);
+
+create table SENSOR
+(
+    Sensor_Id    int,
+    X            float,
+    Y            float,
+    Last_Charged timestamp,
+    Last_Read    timestamp,
+    Constraint sensor_PK primary key (Sensor_Id) not deferrable
+);
+
+create table REPORT
+(
+    Sensor_Id   int,
+    Temperature float,
+    Report_Time timestamp,
+    Constraint report_PK primary key (Sensor_Id, Report_Time) not deferrable,
+    Constraint report_FK foreign key (Sensor_Id) references SENSOR (Sensor_Id) initially deferred deferrable
+);
+
+create table WORKER
+(
+    SSN  varchar(9),
+    Name varchar(30),
+    Age  int,
+    Rank int,
+    Constraint worker_PK primary key (SSN) not deferrable
+);
+
+create table Emergency
+(
+    Sensor_id   int,
+    Report_Time timestamp,
+    constraint emergency_PK primary key (Sensor_Id, Report_Time) deferrable,
+    constraint emergency_FK foreign key (Sensor_Id, Report_Time) references REPORT (Sensor_Id, Report_Time) initially deferred deferrable
+);
+
+commit;
+
+alter table FOREST
+    add Constraint forest_UQ_name UNIQUE (name) initially immediate deferrable;
+
+alter table FOREST
+    add Constraint forest_UQ_MBR UNIQUE (MBR_XMin, MBR_XMax, MBR_YMin, MBR_YMax) initially immediate deferrable;
+
+alter table STATE
+    add Constraint state_UQ_Name UNIQUE (Name) initially immediate deferrable;
+
+alter table SENSOR
+    add Constraint sensor_UQ_coordinate UNIQUE (X, Y) initially immediate deferrable;
+
+alter table SENSOR
+    add energy int not null;
+alter table SENSOR
+    add constraint sensor_energy_check CHECK (energy >= 0 AND energy <= 100);
+
+alter table FOREST
+    add constraint forest_acid_range check (Acid_Level >= 0 and Acid_Level <= 1);
+
+alter table FOREST
+    add Constraint MBRCheck CHECK (MBR_XMax > MBR_XMin and MBR_YMax > MBR_YMin) initially immediate not deferrable;
+
+alter table COVERAGE
+    add Constraint PercentageCheck CHECK (Percentage >= 0 and Percentage <= 1) initially immediate not deferrable;
+
+alter table SENSOR
+    add Maintainer varchar(9) default null;
+
+alter table SENSOR
+    add Constraint sensor_FK foreign key (Maintainer) references WORKER (SSN) initially deferred deferrable;
+
+commit;
+
+INSERT INTO FOREST
+VALUES ('1', 'Allegheny National Forest', 40000.0, 0.3, 134.0, 550.0, 233.0, 598.0);
+INSERT INTO FOREST
+VALUES ('2', 'Pennsylvania Forest', 10000.0, 0.75, 21.0, 100.0, 35.0, 78.0);
+
+INSERT INTO STATE
+VALUES ('Pennsylvania', 'PA', 50000.0, 1400000);
+INSERT INTO STATE
+VALUES ('Ohio', 'OH', 45000.0, 1200000);
+
+INSERT INTO COVERAGE
+VALUES (1, 'PA', 0.4, 16000.0);
+INSERT INTO COVERAGE
+VALUES (1, 'OH', 0.6, 24000.0);
+INSERT INTO COVERAGE
+VALUES (2, 'PA', 1, 10000.0);
+
+INSERT INTO ROAD
+VALUES (1, 'FORBES', 500.0);
+INSERT INTO ROAD
+VALUES (2, 'BIGELOW', 300.0);
+
+INSERT INTO INTERSECTION
+VALUES ('1', '1');
+INSERT INTO INTERSECTION
+VALUES ('1', '2');
+INSERT INTO INTERSECTION
+VALUES ('2', '1');
+INSERT INTO INTERSECTION
+VALUES ('2', '2');
+
+INSERT INTO WORKER
+VALUES ('123456789', 'John', 22, 3);
+INSERT INTO WORKER
+VALUES ('121212121', 'Jason', 30, 5);
+
+INSERT INTO SENSOR
+VALUES (1, 150.0, 300.0, to_timestamp('01-JAN-2019 10:00:00', 'DD-MON-YYYY HH24:MI:SS'),
+        to_timestamp('01-JAN-2019 10:00:00', 'DD-MON-YYYY HH24:MI:SS'), 20, '123456789');
+INSERT INTO SENSOR
+VALUES (2, 200.0, 400.0, to_timestamp('01-JAN-2019 10:00:00', 'DD-MON-YYYY HH24:MI:SS'),
+        to_timestamp('01-JAN-2019 10:00:00', 'DD-MON-YYYY HH24:MI:SS'), 30, '123456789');
+INSERT INTO SENSOR
+VALUES (3, 50.0, 50.0, to_timestamp('01-JAN-2019 10:00:00', 'DD-MON-YYYY HH24:MI:SS'),
+        to_timestamp('01-JAN-2019 10:00:00', 'DD-MON-YYYY HH24:MI:SS'), 30, '121212121');
+INSERT INTO SENSOR
+VALUES (4, 50.0, 15.0, to_timestamp('01-JAN-2019 10:00:00', 'DD-MON-YYYY HH24:MI:SS'),
+        to_timestamp('01-JAN-2019 10:00:00', 'DD-MON-YYYY HH24:MI:SS'), 30, null);
+INSERT INTO SENSOR
+VALUES (5, 60.0, 60.0, to_timestamp('01-JAN-2019 10:00:00', 'DD-MON-YYYY HH24:MI:SS'),
+        to_timestamp('01-JAN-2019 10:00:00', 'DD-MON-YYYY HH24:MI:SS'), 40, '121212121');
+
+INSERT INTO REPORT
+VALUES (1, 55, to_timestamp('10-JAN-2019 09:00:00', 'DD-MON-YYYY HH24:MI:SS'));
+INSERT INTO REPORT
+VALUES (1, 57, to_timestamp('10-JAN-2019 14:00:00', 'DD-MON-YYYY HH24:MI:SS'));
+INSERT INTO REPORT
+VALUES (1, 40, to_timestamp('10-JAN-2019 20:00:00', 'DD-MON-YYYY HH24:MI:SS'));
+INSERT INTO REPORT
+VALUES (2, 58, to_timestamp('10-JAN-2019 12:30:00', 'DD-MON-YYYY HH24:MI:SS'));
+INSERT INTO REPORT
+VALUES (2, 59, to_timestamp('10-JAN-2019 13:00:00', 'DD-MON-YYYY HH24:MI:SS'));
+INSERT INTO REPORT
+VALUES (3, 50, to_timestamp('01-JAN-2019 12:30:00', 'DD-MON-YYYY HH24:MI:SS'));
+INSERT INTO REPORT
+VALUES (4, 30, to_timestamp('01-JAN-2019 22:00:00', 'DD-MON-YYYY HH24:MI:SS'));
+INSERT INTO REPORT
+VALUES (5, 33, to_timestamp('02-JAN-2019 22:00:00', 'DD-MON-YYYY HH24:MI:SS'));
+INSERT INTO REPORT
+VALUES (5, 32, to_timestamp('02-JAN-2019 22:30:00', 'DD-MON-YYYY HH24:MI:SS'));
+
+commit;
